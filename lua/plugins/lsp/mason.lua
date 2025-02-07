f383e291c05e14908c2d1dfcc645788e25c63c95
Weslin Barahona <weslinmbc@gmail.com>
2025-02-07 12:36:47 -0600

    eliminacion erro de carga inicial mason

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,9 +1,20 @@
 return {
+  -- primero se importa el mason
   "williamboman/mason.nvim",
   dependencies = {
-    "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
+
+
+  --Luego se importa por aparte el mason lspcongig porque sino luego da problemas al momento de iniciar
+  {
+    "williamboman/mason-lspconfig.nvim",
+    event = { "BufReadPre", "bufNewFile"},
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+  },
+
   config = function()
     -- import mason
     local mason = require("mason")
@@ -27,10 +38,8 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "tsserver",
         "html",
         "cssls",
-        "tailwindcss",
         "svelte",
         "lua_ls",
         "graphql",
